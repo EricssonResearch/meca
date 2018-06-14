f2b5a42666d62d9c89320067147c68caa72bc904
andre-paraense <andre.paraense@mobwise.com.br>
2018-06-14 12:48:10 -0300

    - api specification for MECA sensory codelets

--- src/br/unicamp/meca/system1/codelets/SensoryCodelet.java
+++ src/br/unicamp/meca/system1/codelets/SensoryCodelet.java
@@ -14,12 +14,31 @@ package br.unicamp.meca.system1.codelets;
 
 import br.unicamp.cst.core.entities.Codelet;
 
-
+/**
+ * This class represents the MECA Sensory Codelets, which are responsible for
+ * grabbing information from sensors at the environment and feeding the
+ * corresponding Memory Objects. Depending on the applications (e.g. robotic
+ * applications), sensory codelets will be really reading the sensor values and
+ * creating a corresponding representation. In other applications (e.g. in a
+ * video-game, an internet agent or a virtual world), sensory codelets will open
+ * sockets to other computer applications and will simulate the acquisition of
+ * data from the environment.
+ * <p>
+ * Usually, Sensory Codelets are application-specific, and the MECA software
+ * implementation just provides basic template class, which is a wrapper to
+ * CST's {@link Codelet}, to be reused while building an application using MECA.
+ * 
+ * @author A. L. O. Paraense
+ * @see Codelet
+ *
+ */
 public abstract class SensoryCodelet extends Codelet {
 
 	protected String id;
 
 	/**
+	 * Creates a MECA Sensory Codelet.
+	 * 
 	 * @param id
 	 */
 	public SensoryCodelet(String id) {
