88b0d6fc57c20a005938f9acb7ffa8309f2ab667
andre-paraense <andre.paraense@mobwise.com.br>
2018-06-15 15:43:18 -0300

    - api specification for motor codelets

--- src/br/unicamp/meca/system1/codelets/MotorCodelet.java
+++ src/br/unicamp/meca/system1/codelets/MotorCodelet.java
@@ -14,13 +14,29 @@ package br.unicamp.meca.system1.codelets;
 
 import br.unicamp.cst.core.entities.Codelet;
 
-
+/**
+ * This class represents the MECA Motor Codelets. Motor codelets simply pick up
+ * the result Memory Object from the Motor Memory and reacts directly at the
+ * environment. This can be done by simply capturing actuator values and feeding
+ * actuators, or by some special protocol interacting with external software or
+ * hardware.
+ * <p>
+ * Usually, Motor Codelets are application-specific, and the MECA software
+ * implementation just provides basic template class, which is a wrapper to
+ * CST's {@link Codelet}, to be reused while building an application using MECA.
+ * 
+ * @author A. L. O. Paraense
+ * @see Codelet
+ */
 public abstract class MotorCodelet extends Codelet {
 
 	protected String id;
 
 	/**
+	 * Creates a MECA Motor Codelet.
+	 * 
 	 * @param id
+	 *            the id of the Motor Codelet. Must be unique per Motor Codelet.
 	 */
 	public MotorCodelet(String id) {
 		super();
@@ -29,6 +45,8 @@ public abstract class MotorCodelet extends Codelet {
 	}
 
 	/**
+	 * Returns the id of this Motor Codelet.
+	 * 
 	 * @return the id
 	 */
 	public String getId() {
@@ -36,7 +54,10 @@ public abstract class MotorCodelet extends Codelet {
 	}
 
 	/**
-	 * @param id the id to set
+	 * Sets the id of this Motor Codelet.
+	 * 
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(String id) {
 		this.id = id;
