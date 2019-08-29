49cab0bf604aa7046cfb19f918066f079bd3c143
Andre Paraense <andre.paraense@mobwise.com.br>
2019-08-29 23:23:50 +0100

    Updating CST and writing ros service test

--- src/test/java/br/unicamp/meca/system1/codelets/rosservice/RosServiceClientTest.java
+++ src/test/java/br/unicamp/meca/system1/codelets/rosservice/RosServiceClientTest.java
@@ -82,4 +82,55 @@ public class RosServiceClientTest {
 		mecaMind.shutDown();
     
     }
+    
+    @Test
+    public void testRosServiceCallTwice() throws URISyntaxException, InterruptedException {
+    	
+		AddTwoIntService addTwoIntService = new AddTwoIntService();
+		NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+		NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic("127.0.0.1",new URI("http://127.0.0.1:11311"));
+		nodeMainExecutor.execute(addTwoIntService, nodeConfiguration);	
+		
+		Thread.sleep(2000);
+		
+		MecaMind mecaMind = new MecaMind("RosServiceClient");
+		
+		List<IMotorCodelet> motorCodelets = new ArrayList<>();
+		
+		AddTwoIntServiceClient addTwoIntServiceClient = new AddTwoIntServiceClient("127.0.0.1",new URI("http://127.0.0.1:11311"));
+		motorCodelets.add(addTwoIntServiceClient);
+    
+		mecaMind.setIMotorCodelets(motorCodelets);
+	    
+	    mecaMind.mountMecaMind();		
+		
+		mecaMind.start();
+		
+		Thread.sleep(5000);
+		
+		Memory motorMemory = addTwoIntServiceClient.getInput(addTwoIntServiceClient.getId());
+		
+		Integer expectedSum = 5;
+		
+		Integer[] numsToSum = new Integer[] {2,3};
+		motorMemory.setI(numsToSum);
+		
+		Thread.sleep(2000);
+		
+		assertEquals(expectedSum, addTwoIntServiceClient.getSum());
+		
+		expectedSum = 6;
+		
+		numsToSum = new Integer[] {3,3};
+		motorMemory.setI(numsToSum);
+		
+		Thread.sleep(2000);
+		
+		assertEquals(expectedSum, addTwoIntServiceClient.getSum());
+		
+		nodeMainExecutor.shutdownNodeMain(addTwoIntService);
+		
+		mecaMind.shutDown();
+    
+    }
 }
