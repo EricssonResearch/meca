3ae08e72fb2eba1a0ea3b615d157e2f2fd46b5f9
Ricardo Gudwin <gudwin@dca.fee.unicamp.br>
2020-10-13 11:55:13 -0300

    Changes to make tests pass in new JDK due to restrictions with just a unique constructor in tests

--- src/test/java/br/unicamp/meca/models/ActionStepTest.java
+++ src/test/java/br/unicamp/meca/models/ActionStepTest.java
@@ -1,17 +1,13 @@
+package br.unicamp.meca.models;
+
+import br.unicamp.cst.core.entities.Memory;
+import br.unicamp.meca.models.ActionStep;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-package br.unicamp.meca.models;
-import br.unicamp.cst.core.entities.Memory;
-import java.util.List;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-
-/**
- *
- * @author rgudwin
  */
 public class ActionStepTest extends ActionStep {
     
@@ -31,23 +27,5 @@ public class ActionStepTest extends ActionStep {
     public boolean stopCondition(List<Memory> perceptions) {
         return(false);
     }
-    
-    @Test
-    public void testActionStep() {
-        ActionStep as1 = new ActionStepTest();
-        assertEquals(as1.getNumberOfParameters(),0);
-        ActionStep as2 = new ActionStepTest("TestAction");
-        assertEquals(as2.getNumberOfParameters(),0);
-        assertEquals(as2.getActionId(),"TestAction");
-        as2.setParameter("param", "value");
-        assertEquals(as2.getNumberOfParameters(),1);
-        assertEquals(as2.getParameter("param"),"value");
-        as2.setParameter("param", "value2");
-        assertEquals(as2.getParameter("param"),"value2");
-        as2.unsetParameter("param");
-        assertEquals(as2.getNumberOfParameters(),0);
-    }
-
-
     
 }
